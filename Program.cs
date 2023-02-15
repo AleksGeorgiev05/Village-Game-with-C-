26d4a4b9748c7ad75d4da1ed6a11ce1e01823787
AleksGeorgiev05 <100018962+AleksGeorgiev05@users.noreply.github.com>
2023-02-15 18:06:44 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+class StartUp
+{
+    static void Main(string[] args)
+    {
+        Controller controller = new Controller();
+        Engine engine = new Engine(controller);
+
+        engine.Run();
+        Console.ReadKey();
+    }
+}
---
+++ Rebel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+public class Rebel : Person
+{
+    private int harm;
+    public int Harm
+    {
+        get { return harm; }
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Harm should be greater than 0!");
+            }
+            harm = value;
+        }
+    }
+
+
+
+    public Rebel(string name, int age, int harm) : base(name, age)
+    {
+        if (age > 50)
+        {
+            throw new ArgumentException("Age should be less or equel to 50!");
+        }
+        this.harm = harm;
+    }
+    public override string ToString()
+    {
+        return base.ToString() + "\n" + $"Harm: {this.Harm}";
+    }
+}
